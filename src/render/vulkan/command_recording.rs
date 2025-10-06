7084bb7bff91b1d14c6a8836cd898eaa08f74b88
Nikita Kladov <vastausf@gmail.com>
2025-10-06 18:54:19 +0200

    ALume-6: Add per-frame command buffers

--- src/render/vulkan/command_recording.rs
+++ src/render/vulkan/command_recording.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use ash::vk::{CommandBufferResetFlags, CommandBufferUsageFlags};
 use ash::{Device, vk};
 use std::slice;
 use tracing::{debug, instrument, trace};
@@ -9,140 +10,95 @@ use vk::{
 };
 
 pub struct CommandRecording {
-    pub pool: CommandPool,
-    pub buffers: Vec<CommandBuffer>,
-    clear: [f32; 4],
+    pub command_pool: CommandPool,
+    pub command_buffer: CommandBuffer,
+
+    pub clear: [f32; 4],
 }
 
 impl CommandRecording {
-    pub fn allocate_and_record(
-        device: &Device,
-        family: u32,
-        render_pass: RenderPass,
-        frame_buffers: &[Framebuffer],
-        extent: Extent2D,
-        clear: [f32; 4],
-    ) -> Result<Self> {
+    pub fn create(device: &Device, queue_family: u32, clear: [f32; 4]) -> Result<Self> {
         let command_pool_create_info = CommandPoolCreateInfo::default()
-            .queue_family_index(family)
+            .queue_family_index(queue_family)
             .flags(CommandPoolCreateFlags::RESET_COMMAND_BUFFER);
         debug!("Creating CommandPool...");
         let command_pool = unsafe { device.create_command_pool(&command_pool_create_info, None)? };
         debug!("CommandPool created. OK");
 
-        if frame_buffers.is_empty() {
-            debug!("No FrameBuffers. Skipping CommandBuffer allocation/record");
-            return Ok(Self {
-                pool: command_pool,
-                buffers: Vec::new(),
-                clear,
-            });
-        }
-
         let command_buffer_allocate_info = CommandBufferAllocateInfo::default()
             .command_pool(command_pool)
             .level(CommandBufferLevel::PRIMARY)
-            .command_buffer_count(frame_buffers.len() as u32);
+            .command_buffer_count(1);
         debug!("Allocating CommandBuffers...");
-        let command_buffers =
-            unsafe { device.allocate_command_buffers(&command_buffer_allocate_info)? };
+        let command_buffer =
+            unsafe { device.allocate_command_buffers(&command_buffer_allocate_info)?[0] };
         debug!("CommandBuffers allocated. OK");
 
-        Self::record_all(
-            &device,
-            render_pass,
-            &command_buffers,
-            frame_buffers,
-            extent,
-            clear,
-        )?;
-
         Ok(Self {
-            pool: command_pool,
-            buffers: command_buffers,
+            command_pool,
+            command_buffer,
+
             clear,
         })
     }
 
-    #[instrument(skip_all)]
-    pub fn reallocate_and_record(
-        &mut self,
-        device: &Device,
-        render_pass: RenderPass,
-        frame_buffers: &[Framebuffer],
-        extent: Extent2D,
-    ) -> Result<()> {
-        trace!("Freeing CommandBuffers...");
+    #[instrument(level = "trace", skip_all)]
+    pub fn reset_begin_one_time(&self, device: &Device) -> Result<()> {
         unsafe {
-            device.free_command_buffers(self.pool, &self.buffers);
+            device.reset_command_buffer(self.command_buffer, CommandBufferResetFlags::empty())?
         }
-        trace!("CommandBuffers freed. OK");
-
-        let command_buffer_allocate_info = CommandBufferAllocateInfo::default()
-            .command_pool(self.pool)
-            .level(CommandBufferLevel::PRIMARY)
-            .command_buffer_count(frame_buffers.len() as u32);
-        trace!("Allocating CommandBuffers...");
-        self.buffers = unsafe { device.allocate_command_buffers(&command_buffer_allocate_info)? };
-        trace!("CommandBuffers allocated. OK");
-
-        Self::record_all(
-            &device,
-            render_pass,
-            &self.buffers,
-            frame_buffers,
-            extent,
-            self.clear,
-        )
-    }
-
-    fn record_all(
-        device: &Device,
-        render_pass: RenderPass,
-        command_buffers: &[CommandBuffer],
-        frame_buffers: &[Framebuffer],
-        extent: Extent2D,
-        clear: [f32; 4],
-    ) -> Result<()> {
-        for (command_buffer, &frame_buffer) in command_buffers.iter().zip(frame_buffers) {
-            unsafe {
-                debug!("Begin recording command buffer...");
-                device.begin_command_buffer(*command_buffer, &CommandBufferBeginInfo::default())?;
-
-                let clear_value = ClearValue {
-                    color: ClearColorValue { float32: clear },
-                };
-
-                let render_pass_begin_info = RenderPassBeginInfo::default()
-                    .render_pass(render_pass)
-                    .framebuffer(frame_buffer)
-                    .render_area(Rect2D {
-                        offset: Offset2D { x: 0, y: 0 },
-                        extent,
-                    })
-                    .clear_values(slice::from_ref(&clear_value));
-                trace!("Begin render recording...");
-                device.cmd_begin_render_pass(
-                    *command_buffer,
-                    &render_pass_begin_info,
-                    SubpassContents::INLINE,
-                );
-                trace!("Render pass begun");
-
-                trace!("Ending render pass...");
-                device.cmd_end_render_pass(*command_buffer);
-                device.end_command_buffer(*command_buffer)?;
-                trace!("Render pass ended");
-            }
-        }
+        let command_buffer_begin_info =
+            CommandBufferBeginInfo::default().flags(CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+        unsafe { device.begin_command_buffer(self.command_buffer, &command_buffer_begin_info)? }
 
         Ok(())
     }
 
+    #[instrument(level = "trace", skip_all)]
+    pub fn record_pass(
+        &self,
+        device: &Device,
+        render_pass: RenderPass,
+        framebuffer: Framebuffer,
+        extent: Extent2D,
+    ) -> Result<()> {
+        let clear_value = ClearValue {
+            color: ClearColorValue {
+                float32: self.clear,
+            },
+        };
+
+        let render_pass_begin_info = RenderPassBeginInfo::default()
+            .render_pass(render_pass)
+            .framebuffer(framebuffer)
+            .render_area(Rect2D {
+                offset: Offset2D { x: 0, y: 0 },
+                extent,
+            })
+            .clear_values(slice::from_ref(&clear_value));
+        trace!("Begin render recording...");
+        unsafe {
+            device.cmd_begin_render_pass(
+                self.command_buffer,
+                &render_pass_begin_info,
+                SubpassContents::INLINE,
+            )
+        }
+        trace!("Render pass begun");
+
+        trace!("Ending render pass...");
+        unsafe { device.cmd_end_render_pass(self.command_buffer) }
+        unsafe { device.end_command_buffer(self.command_buffer)? }
+        trace!("Render pass ended");
+
+        Ok(())
+    }
+
+    #[instrument(level = "trace", skip_all)]
     pub fn destroy(&self, device: &Device) {
         unsafe {
-            device.free_command_buffers(self.pool, &self.buffers);
-            device.destroy_command_pool(self.pool, None);
+            device.free_command_buffers(self.command_pool, &[self.command_buffer]);
+            device.destroy_command_pool(self.command_pool, None);
         }
     }
 }
--- src/render/vulkan/frame_sync.rs
+++ src/render/vulkan/frame_sync.rs
@@ -1,3 +1,5 @@
+use crate::render::vulkan::command_recording::CommandRecording;
+use crate::render::vulkan::queue_families::QueueFamilies;
 use anyhow::Result;
 use ash::Device;
 use ash::vk::{Fence, FenceCreateFlags, FenceCreateInfo, Semaphore, SemaphoreCreateInfo};
@@ -7,10 +9,12 @@ pub struct FrameSync {
 
     pub image_available: Semaphore,
     pub render_finished: Semaphore,
+
+    pub command_recording: CommandRecording,
 }
 
 impl FrameSync {
-    pub fn create(device: &Device) -> Result<Self> {
+    pub fn create(device: &Device, queue_families: &QueueFamilies) -> Result<Self> {
         let fence_create_info = FenceCreateInfo::default().flags(FenceCreateFlags::SIGNALED);
         let fence = unsafe { device.create_fence(&fence_create_info, None)? };
 
@@ -18,18 +22,26 @@ impl FrameSync {
         let image_available = unsafe { device.create_semaphore(&semaphore_create_info, None)? };
         let render_finished = unsafe { device.create_semaphore(&semaphore_create_info, None)? };
 
+        let command_recording =
+            CommandRecording::create(&device, queue_families.graphics, [0.08, 0.10, 0.15, 1.0])?;
+
         Ok(Self {
             fence,
 
             image_available,
             render_finished,
+
+            command_recording,
         })
     }
 
     pub fn destroy(&self, device: &Device) {
         unsafe {
+            self.command_recording.destroy(device);
+
             device.destroy_semaphore(self.image_available, None);
             device.destroy_semaphore(self.render_finished, None);
+
             device.destroy_fence(self.fence, None);
         }
     }
